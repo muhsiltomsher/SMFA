708cc20aca71b038348d2220ab585f3c750c7636
muhsiltomsher <muhsil@tomsher.com>
2025-09-16 11:40:38 +0400

    dfs

--- resources/views/sections/home/hero.blade.php
+++ resources/views/sections/home/hero.blade.php
@@ -1,6 +1,6 @@
 <x-hero-section widthClass="max-w-[1250px]">
     The Private Office is the principal vehicle for managing, preserving, and enhancing the <br> wealth and legacy of
-    H.H. Sheikh Mohammed bin Faisal bin Sultan Al Qassimi. <br> Established to facilitate long-term sustainability and
-    growth, the office provides a centralised  <br> approach to overseeing investments, governance, and multigenerational
+    H.H. Sheikh Mohammed bin Faisal bin Sultan Al Qassimi. Established <br>  to facilitate long-term sustainability and
+    growth, the office provides a centralised  platform <br> for overseeing investments, governance, and multigenerational
     wealth planning.
 </x-hero-section>
